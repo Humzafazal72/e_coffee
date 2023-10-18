cb394c2fdf3dfdf3a9446381d164604772e50966
Humza <abbasisaud99@gmail.com>
2023-10-18 23:30:19 +0500

    this commit was a mistake

--- index/views.py
+++ index/views.py
@@ -60,21 +60,21 @@ def cart(request):
         sub_total=sum(total_all)
         sub_total=Decimal(sub_total)
 
+        id=random.randint(1,100000000)
+        Order=order.objects.create(id=id,cart=cart,total=total)
+
         shipping=Decimal(10)
         if sub_total==0:
             shipping=0
                 
         total=sub_total+shipping
 
-        id=random.randint(1,100000000)
-        Order=order.objects.create(id=id,cart=cart,total=total)
-
         return render(request, 'cart.html',{'cartitem':Cartitems,'sub':sub_total,
                                             'total':total,'order_id':Order.id,'shipping':shipping})
     
     return redirect('shop')
 
-#adding an item to cart
+
 def add_to_cart(request,name):
     if request.user.is_authenticated:
         customer=customers.objects.get(pk=request.user.username)
@@ -92,7 +92,7 @@ def add_to_cart(request,name):
 
         return redirect('shop')
     
-#remove an item from cart
+
 def remove_coffee(request,name):
     customer=customers.objects.get(pk=request.user.username)
     cart=Cart.objects.get(user=customer)
@@ -103,11 +103,9 @@ def remove_coffee(request,name):
 
     return redirect('cart')
 
-#online payment
 def checkout(request,order_id):
     return HttpResponse("Work going on.....")
 
-#Emptying cart after user selects cash on delivery
 def finalize(request,order_id):
     Order=order.objects.get(id=order_id)
     cart=Order.cart
